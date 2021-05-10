d820d124237995ba3a8c6fb3603d5263eb1a67c5
DanielMolloyUON <c3311882@uon.edu.au>
2021-05-10 13:52:55 +1000

    created a starter navbar with icons

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
 import './Header.css'
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import MessageIcon from '@material-ui/icons/Message';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
@@ -10,24 +14,28 @@ function Header() {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1000px-Amazon_logo.svg.png"></img>
 
             <div className='header__search'>
-                <input className ='header_searchInput' type="text"/>
-                <SearchIcon/>
+                <input className ='header__searchInput' type="text"/>
+                <SearchIcon className="header__searchIcon"/>
             </div>
 
             {/* Logo */}
 
             <div className='header__nav'>
                 <div className="header__option">
-                    Home
+                    <HomeIcon className="header__optionIcon"/>
+                    <div className="header__optionText">Home</div>
                 </div>
                 <div className="header__option">
-                    Orders
+                    <ShoppingCart className="header__optionIcon"/>
+                    <div className="header__optionText">Orders</div>
                 </div>
                 <div className="header__option">
-                    Messaging
+                    <MessageIcon className="header__optionIcon"/>
+                    <div className="header__optionText">Messaging</div>
                 </div>
                 <div className="header__option">
-                    Notifications
+                    <NotificationsIcon className="header__optionIcon"/>
+                    <div className="header__optionText">Notifications</div>
                 </div>
             </div>
         </div>

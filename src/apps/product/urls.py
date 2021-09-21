4d811414d4061e6752a87c54c875463d8bbfa804
DanielMolloyUON <c3311882@uon.edu.au>
2021-09-22 08:41:13 +1000

    working 2

--- src/apps/product/urls.py
+++ src/apps/product/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('search/',views.search, name='search'),
+#    path('', views.autocomplete, name='autocomplete'),
     path('category_list/',views.show_category, name='category_list'),
     path('<slug:category_slug>/<slug:product_slug>/', views.product, name='product'), # create the url for a detailed product view from its category and product url slug
     path('<slug:category_slug>/', views.category, name='category'),
---
+++ src/static/scripts/autocomplete.js
@@ -0,0 +1,5 @@
+$( function() {
+    $( "#device" ).autocomplete({
+      source: availableTags
+    });
+});

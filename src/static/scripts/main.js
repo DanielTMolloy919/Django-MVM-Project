e91f741c4fd380b64beaed2504ee0a3d551e6199
Daniel Molloy <c3311882@uon.edu.au>
2022-04-14 22:45:42 +1000

    Working nicely jsed modal

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,15 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+document.addEventListener("DOMContentLoaded", () => {
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
-    if ($navbarBurgers.length > 0) {
-        $navbarBurgers.forEach( el => {
-            el.addEventListener('click', () => {
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-            });
-        });
-    }
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+
+  $(".button").click(function () {
+    var buttonId = $(this).attr("id");
+    $("#modal-container").removeAttr("class").addClass(buttonId);
+    $("body").addClass("modal-active");
+  });
+
+  $("#modal-container").click(function () {
+    $(this).addClass("out");
+    $("body").removeClass("modal-active");
+  });
 });
